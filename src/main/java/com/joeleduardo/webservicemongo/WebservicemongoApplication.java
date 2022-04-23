fcec40d00221a007da333b15d46335c4831e6a97
Joel Eduardo <joeleduardo39@gmail.com>
2022-04-23 18:48:08 -0300

    Projeto criado

---
+++ src/main/java/com/joeleduardo/webservicemongo/WebservicemongoApplication.java
@@ -0,0 +1,13 @@
+package com.joeleduardo.webservicemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicemongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicemongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joeleduardo/webservicemongo/WebservicemongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeleduardo.webservicemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicemongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
